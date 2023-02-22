7fe499aeee3443fa78742c8390a4f993f47e3e9e
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-02-22 11:29:20 -0800

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * File: main.h
- * Auth: hussein hassan
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int num1, int num2, int num3);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
