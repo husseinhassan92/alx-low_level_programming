4934a1d317729b3a72460d9dfbaeac35ede5409d
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-02-27 12:43:47 -0800

    inital commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int i, len = 0;
+
+	while (s[i])
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
