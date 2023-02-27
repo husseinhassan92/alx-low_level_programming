44f3a1dca4603b586d782f45d5c081d083fdb93d
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-02-27 13:07:30 -0800

    inital commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,18 +5,26 @@
  */
 void print_rev(char *s)
 {
-	int i, len = 0;
 
+	int len = 0;
+	int i;
+
+	/* iterate to find length of string and point to last character */
 	while (*s != '\0')
 	{
 		len++;
 		++s;
 	}
+
+	/* go back to character before null character */
 	s--;
-	for (i = len; i >= 0; i--)
+
+	/* print string reversed */
+	for (i = len; i > 0; i--)
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
