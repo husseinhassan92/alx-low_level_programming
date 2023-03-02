0369abb8ef625ef69ac88c8dde5bd6684dd076b4
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-02 07:01:16 -0800

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,23 +4,29 @@
  * @s: string to manipulate
  * Return: string
  */
+
 char *cap_string(char *s)
 {
 	int i = 0;
 
-	if (s[i] >= 'a'; s[i] <= 'z')
+	/* check first index for capital */
+	if (s[i] >= 'a' && s[i] <= 'z')
 		s[i] = s[i] - 'a' + 'A';
 	i++;
-	while (s[i] != '\0')
+
+	while (s[i] != '\0') /* iterate through string */
 	{
+
+		/* if lowercase and prior char is separator, capitalize*/
 		if ((s[i] >= 'a' && s[i] <= 'z')
-				&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-					s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-					s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-					s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
-					|| s[i - 1] == '\n'))
-			s[i] = s[i] - 'a' + 'A'
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
 		i++;
 	}
+
 	return (s);
 }
