e378ca3e0a3332cf8f3f776ae881816ac1166895
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-02 00:37:06 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,15 @@ char *_strcat(char *dest, char *src)
 	int j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
+
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
+		dest[i] src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
-	return (dist);
+
+	return (dest);
 }
