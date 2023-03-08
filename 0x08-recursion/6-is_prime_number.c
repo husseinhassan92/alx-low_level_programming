f9f97e977817d43b3124a121c5e535636b1086f5
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-08 13:06:32 -0800

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,3 +15,20 @@ int is_prime(int n, int divisor)
 	return (is_prime(n, divisor + 1));
 
 }
+/**
+ * is_prime_number - check if prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	int divisor = 3;
+
+	if (n % 2 == 0 || n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+
+	return (is_prime(n, divisor));
+}
