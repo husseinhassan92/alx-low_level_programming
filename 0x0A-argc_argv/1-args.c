d4b94c8f98965444c639a4d320accd3564aef9f9
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-10 08:31:33 -0800

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio>
+#include "main.h"
+/**
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc);
+	return (0);
+}
