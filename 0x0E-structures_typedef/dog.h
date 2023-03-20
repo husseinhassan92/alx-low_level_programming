b3a1ceeedb7622e01b6cd4668ce4057633608c40
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-20 11:05:20 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 /**
+ * dog - Typedef for struct dog
+ */
+typedef struct dog dog;
+/**
  * struct dog - structure for dogs
  * @name: first member
  * @age: second member
  * @owner: third member
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
