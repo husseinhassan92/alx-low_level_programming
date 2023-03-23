5c1bf19071371e3f34014103192428e3a0be655a
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-23 04:18:01 -0700

    task1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
-	unsigned int i, sum = 0;
+	va_list valist;
+	unsigned int sum = 0, i;
 
+	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
-	va_start(list, n);
-	for (i = 0, i < n, i++)
-		sum += va_arg(list, int);
-	va_end(list);
+	va_start(valist, n);
+
+	/* iterate through list, update sum, free list */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+
 	return (sum);
 }
