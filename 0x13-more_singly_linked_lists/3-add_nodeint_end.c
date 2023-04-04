9d4941f5179ed1b166bf84c4eb69ec59fa58effb
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-04-04 05:06:41 -0700

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,25 +10,25 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
-	listint_t *new_node, *tmp;
+	listint_t *node, *temp;
 
-	new_node = malloc(sizeof(listint_t)); /* make new node and set values */
-	if (new_node == NULL)
+	node = malloc(sizeof(listint_t)); /* make new node and set values */
+	if (node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
+	node->n = n;
+	node->next = NULL;
 
 	if (*head == NULL) /* account for empty list by pointing head to node */
-		*head = new_node;
+		*head = node;
 	else
 	{
-		tmp = *head; /* set tmp ptr to iterate w/o moving head ptr */
+		temp = *head; /* set tmp ptr to iterate w/o moving head ptr */
 
-		while (tmp->next != NULL) /* iterate till last node */
-			tmp = tmp->next;
-		tmp->next = new_node; /* link last node to new node */
+		while (temp->next != NULL) /* iterate till last node */
+			temp = temp->next;
+		temp->next = node; /* link last node to new node */
 	}
 
-	return (new_node);
+	return (node);
 }
