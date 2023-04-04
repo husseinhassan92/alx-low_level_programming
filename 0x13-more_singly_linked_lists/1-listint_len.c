73ea2d827b4535611961dbd0b8ed45d24af1aef5
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-04-04 04:35:13 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t num_nodes = 0;
+	size_t node = 0;
 
 	while (h != NULL)
 	{
-		num_nodes += 1;
+		node += 1;
 		h = h->next;
 	}
 
-	return (num_nodes);
+	return (node);
 }
