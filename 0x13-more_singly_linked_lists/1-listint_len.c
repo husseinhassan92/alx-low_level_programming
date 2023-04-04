8867ede96e5a314505ba13aeeba74edac67cc687
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-04-04 04:29:09 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * listint_len - returns the number of elements in a linked listint_t list
  * @h: list
@@ -6,12 +7,15 @@
  */
 size_t listint_len(const listint_t *h)
 {
+	int count = 0;
 	size_t node = 0;
 
 	if (h != NULL)
 	{
+		count++;
 		node++;
 		h = h->next;
 	}
+	printf("%d\n", count);
 	return (node);
 }
