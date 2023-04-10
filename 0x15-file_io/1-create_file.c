90d2500d90ed772cb2a11024cf796f513527d96e
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-04-10 06:30:12 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
  * create_file - creates file with permissions rw------- and writes content in
  * if file already exists, don't change permissions and just truncate it
  * @filename: name to give to new file
@@ -8,24 +23,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int open, write, str_len;
+	int fd;
+	int w;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (str_len = 0; text_content[str_len];)
-			str_len++;
-	}
-
-	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write = write(open, text_content, str_len);
-
-	if (open == -1 || write == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	close(open);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 
+	w = write(fd, text_content, _strlen(text_content));
+	if (w == -1 || w != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
 	return (1);
 }
