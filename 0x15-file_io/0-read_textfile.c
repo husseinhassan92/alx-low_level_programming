66d62a15f3cd85d374a73ea15eb81de45118af65
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-04-10 05:57:31 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,28 +8,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t open, read, write;
+	int fd;
+	ssize_t read, write;
 	char *buffer;
 
 	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	open = open(filename, O_RDONLY);
-	read = read(o, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, r);
-
-	if (open == -1 || read == -1 || write == -1 || write != read)
+	read = read(fd, buffer, letters);
+	if (read == -1)
 	{
 		free(buffer);
+		close(fd);
 		return (0);
 	}
 
-	free(buffer);
-	close(open);
+	write = write(STDOUT_FILENO, buffer, read);
+	if (wrote == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	return (write);
+	close(fd);
+	return (read);
+
 }
