885bf25c45f0aa832b882b59d51ae8fc3bd7f188
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-05-18 19:07:35 +0300

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a doubly linked list with only int data, no strings
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (head->next != NULL)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
