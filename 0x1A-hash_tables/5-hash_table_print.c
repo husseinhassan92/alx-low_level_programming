af47ad51011a61b52dd98e27ce75973d52bad46e
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-06-09 19:13:59 +0300

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print key/values of hash table in order
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *node;
+	char *sep = "";
+
+	if (!ht || !ht->array)
+		return;
+
+	printf('{');
+	while (idx < ht->size)
+	{
+		node = ((ht->array)[idx]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
+			node = node->next;
+		}
+		idx++;
+	}
+	printf("}");
+}
