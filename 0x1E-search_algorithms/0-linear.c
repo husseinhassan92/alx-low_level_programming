61eb177fa2466554a94e515b5d09d67a8bd11295
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-12 15:46:45 +0300

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+# include "search_algos.h"
+
+
+/**
+ * linear_search - search linearly
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1)
+}
