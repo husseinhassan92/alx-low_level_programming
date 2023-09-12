40bb256414cf8bc1120479dad24ea8899feb1b18
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-09-12 17:28:49 +0300

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+# include "search_algos.h"
+
+
+/**
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t mid, i;
+	size_t right = size - 1;
+
+	while (!array)
+		return(-1);
+	while (left <= right)
+	{
+		for (i = left; i <= right; i++)
+		{
+			printf("Searching in array: ");
+			if (left < right)
+				printf("%d ", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+		mid = (left + right) / 2;
+		if (array[mid] > value)
+			right = mid - 1;
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			return (mid);
+	}
+	return(-1);
+}
